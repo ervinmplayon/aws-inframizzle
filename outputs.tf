4b96755a4efeee1c852f18de1e0e010f6fe38bce
ervz <ervin.mitra@playonsports.com>
2025-04-16 11:01:43 -0400

    output the instance public ip

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "instance_id" {
     description = "ID of the EC2 Instance"
     value = aws_instance.app_server.id
 }
+
+output "instance_public_ip" {
+    description = "Public IP address of the EC2 Instance"
+    value = aws.instance.app_server.instance_public_ip
+}
